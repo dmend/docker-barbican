983dcc6cfd5acef475d43c90d6fa45327a34a41b
Douglas Mendizábal <douglas@redrobot.io>
2017-01-18 23:10:20 -0600

    Use openstack cli to bootstrap keystone

---
+++ lib/osauth.sh
@@ -0,0 +1,8 @@
+export OS_IDENTITY_API_VERSION=3
+export OS_AUTH_URL=http://localhost:5000/v3
+export OS_AUTH_TYPE=v3password
+export OS_PROJECT_NAME=admin
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_USERNAME=admin
+export OS_PASSWORD=PASSWORD
+export OS_USER_DOMAIN_NAME=Default
